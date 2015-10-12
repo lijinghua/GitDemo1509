8b2d4181dcef698882f41a5da7fd1594334434c4
ljh <lijh@test.com>
2015-10-12 11:49:59 +0800

    Second commit

---
+++ GitDemo/GitDemo/SecondObject.h
@@ -0,0 +1,13 @@
+//
+//  SecondObject.h
+//  GitDemo
+//
+//  Created by lijinghua on 15/10/12.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SecondObject : NSObject
+
+@end
---
+++ GitDemo/GitDemo/SecondObject.m
@@ -0,0 +1,13 @@
+//
+//  SecondObject.m
+//  GitDemo
+//
+//  Created by lijinghua on 15/10/12.
+//  Copyright © 2015年 lijinghua. All rights reserved.
+//
+
+#import "SecondObject.h"
+
+@implementation SecondObject
+
+@end
